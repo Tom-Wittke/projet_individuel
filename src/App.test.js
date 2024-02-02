92bf219dc7e40f16ea6d9bf6b80dca632af7abb4
Tom-Wittke <wtom@live.fr>
2024-02-02 14:59:59 +0100

    Init project

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,13 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("check counter on click me button", () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  const linkElement = screen.getByRole('link');
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toHaveTextContent(/learn react/i);
-});
-
-test('renders code app', () => {
-  render(<App />);
-  const codeAppElement = screen.getByTestId('code-app');
-  expect(codeAppElement).toBeInTheDocument();
-  expect(codeAppElement).toHaveTextContent("src/App.js");
-});
-
-test('check counter on click me button', () => {
-  render(<App />);
-  const button = screen.getByRole('button');
-  const counter = screen.getByTestId('count')
+  const button = screen.getByRole("button");
+  const counter = screen.getByTestId("count");
   expect(button).toBeInTheDocument();
   expect(counter).toBeInTheDocument();
   expect(counter).toHaveTextContent("0");
   fireEvent.click(button);
   expect(counter).toHaveTextContent("1");
 });
-
--- src/module.js
+++ src/module.js
@@ -1,19 +1,26 @@
 /**
  * Calculate a person's age in years.
  *
- * @param {object} p An object representing a person, implementing a birth Date parameter.
- * @return {number} The age in years of p.
+ * @param {object}  An object representing a person, implementing a birth Date parameter.
+ * @return {number} The age in years of p
  */
-export function calculateAge(p) {
-    if(!p) {
-        throw new Error("missing param p")
-    }
-    let dateDiff = new Date(Date.now() - p.birth.getTime())
-    let age = Math.abs(dateDiff.getUTCFullYear() - 1970);
-    return age;
-}
 
-// let loise = {
-//     birth: new Date("11/07/1991")
-// }
-// console.log(calculateAge(loise))
+import { isDate } from "util/types";
+
+export function calculateAge(p) {
+  if (!p) {
+    throw new Error("missing param p");
+  } else if (!(typeof p === "object")) {
+    throw new Error("it's not an object");
+  } else if (p.birth == null) {
+    throw new Error("birth is missing in the object");
+  } else if (!(p.birth instanceof Date)) {
+    throw new Error("birth is not a date");
+  } else if (isNaN(p.birth)) {
+    throw new Error("wrong date");
+  }
+
+  let dateDiff = new Date(Date.now() - p.birth.getTime());
+  let age = Math.abs(dateDiff.getUTCFullYear() - 1970);
+  return age;
+}
